7efcc110ed0529581cdba51619be6bb99f192393
Durgaprasad Nagarkatte <durgaprasad.nagarkatte@gmail.com>
2017-01-13 18:09:39 +0530

    Initial Commit

---
+++ app/src/main/java/com/dpcompany/gameconnect/MainActivity.java
@@ -0,0 +1,35 @@
+package com.dpcompany.gameconnect;
+
+import android.media.Image;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    int activePlayer = 0;
+    int[] gameState = {2, 2, 2, 2, 2, 2, 2, 2, 2};
+
+    public void dropIn(View view) {
+        ImageView counter = (ImageView) view;
+        System.out.println(counter.getTag().toString());
+        counter.setTranslationY(-1000f);
+        if(activePlayer == 0) {
+            counter.setImageResource(R.drawable.yellow);
+            counter.animate().translationYBy(100f).rotation(360).setDuration(300);
+            activePlayer = 1;
+        }
+        else{
+            counter.setImageResource(R.drawable.red);
+            counter.animate().translationYBy(100f).rotation(360).setDuration(300);
+            activePlayer = 0;
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
