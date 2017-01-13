1ab96526c883a9d3ce6180231055baec54e71704
Durgaprasad Nagarkatte <durgaprasad.nagarkatte@gmail.com>
2017-01-13 19:19:18 +0530

    Adding Play Again

--- app/src/main/java/com/dpcompany/gameconnect/MainActivity.java
+++ app/src/main/java/com/dpcompany/gameconnect/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class
 MainActivity extends AppCompatActivity {
@@ -36,7 +37,8 @@ MainActivity extends AppCompatActivity {
                         gameState[winningPosition[1]] == gameState[winningPosition[2]] &&
                         gameState[winningPosition[0]] != 2) {
                     System.out.println(gameState[winningPosition[0]]);
-
+                    LinearLayout layout = (LinearLayout)findViewById(R.id.playAgainLayout);
+                    layout.setVisibility(View.VISIBLE);
                 }
             }
         }
