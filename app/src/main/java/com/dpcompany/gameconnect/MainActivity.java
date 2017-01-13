64184f1a67cdb5507fd925f78c0333ae277a3f8e
Durgaprasad Nagarkatte <durgaprasad.nagarkatte@gmail.com>
2017-01-13 19:00:43 +0530

    Basic game build

--- app/src/main/java/com/dpcompany/gameconnect/MainActivity.java
+++ app/src/main/java/com/dpcompany/gameconnect/MainActivity.java
@@ -6,16 +6,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class
+MainActivity extends AppCompatActivity {
 
     int activePlayer = 0;
     int[] gameState = {2, 2, 2, 2, 2, 2, 2, 2, 2};
+    int[][] winningPositions = {{0,1,2}, {3,4,5},{6,7,8}, {0,3,6}, {1,4,7}, {2,5,8},{0,4,8}, {2,4,6}};
 
     public void dropIn(View view) {
         ImageView counter = (ImageView) view;
-
         System.out.println(counter.getTag().toString());
         int tappedCounter = Integer.parseInt(counter.getTag().toString());
+
         if (gameState[tappedCounter] == 2) {
             gameState[tappedCounter] = activePlayer;
             counter.setTranslationY(-1000f);
@@ -23,10 +25,19 @@ public class MainActivity extends AppCompatActivity {
                 counter.setImageResource(R.drawable.yellow);
                 counter.animate().translationYBy(100f).rotation(360).setDuration(300);
                 activePlayer = 1;
-            } else {
+            }
+            else {
                 counter.setImageResource(R.drawable.red);
                 counter.animate().translationYBy(100f).rotation(360).setDuration(300);
                 activePlayer = 0;
+            }
+            for(int[] winningPosition: winningPositions){
+                if(gameState[winningPosition[0]] == gameState[winningPosition[1]] &&
+                        gameState[winningPosition[1]] == gameState[winningPosition[2]] &&
+                        gameState[winningPosition[0]] != 2) {
+                    System.out.println(gameState[winningPosition[0]]);
+
+                }
             }
         }
     }
